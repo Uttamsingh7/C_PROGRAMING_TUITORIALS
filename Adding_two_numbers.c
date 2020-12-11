2f04c6f1e41235c4dfd8b731915db6bedf3937a0
Uttamsingh7 <uttambsdc2@gmail.com>
2020-12-11 02:39:20 -0500

    Added spaces

--- Adding_two_numbers.c
+++ Adding_two_numbers.c
@@ -14,10 +14,14 @@
 void main()
 {
 	int num1, num2,sum;
+
 	printf("Enter the numbers num1, num2:");
 	scanf("%d", &num1);
 	scanf("%d", &num2);
+
 	sum = num1 + num2;
+
 	printf ("sum of num1= %d, num2=%d is %d\n", num1, num2, sum);
+
 }
 
