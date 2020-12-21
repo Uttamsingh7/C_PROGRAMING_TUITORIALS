906aac9a5695b2e822b76d96aac1ea1255dbf605
Uttamsingh7 <uttambsdc2@gmail.com>
2020-12-21 13:39:25 -0500

    adding description

--- Printing_all_odd_numbers_1_to_100.c
+++ Printing_all_odd_numbers_1_to_100.c
@@ -1,3 +1,14 @@
+/*
+*      File Name: Printin_all_odd_numbers.c
+*      Date:22/12/2020
+*
+*      Tuitor: Uttam Kumar 
+*
+*      Email Id: uttambsdc2@gmail.com
+*/
+
+/* Description: C program to printing all odd numbers 1 to 100. */
+
 #include <stdio.h>
 int main ()
 {
