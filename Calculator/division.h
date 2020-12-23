a761aad479cfbbb9f2472cdad6d61d6ca7254e1b
Uttamsingh7 <uttambsdc2@gmail.com>
2020-12-23 05:53:55 -0500

    Calculator

---
+++ Calculator/division.h
@@ -0,0 +1,12 @@
+/*
+*      File Name: division.h
+*      Date:23/12/2020
+*
+*      Tuitor: Uttam Kumar 
+*
+*      Email Id: uttambsdc2@gmail.com
+*/
+
+/* Description: C program to implement Calculator. */
+
+int division (int a, int b);
---
+++ Calculator/multiply.h
@@ -0,0 +1,13 @@
+
+/*
+*      File Name: multiply.h
+*      Date:23/12/2020
+*
+*      Tuitor: Uttam Kumar 
+*
+*      Email Id: uttambsdc2@gmail.com
+*/
+
+/* Description: C program to implement Calculator. */
+
+int multiply(int a, int b);
