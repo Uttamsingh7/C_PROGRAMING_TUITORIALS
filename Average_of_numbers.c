6c094a609c66c1846d8f20dd396484d97ad237f8
Uttamsingh7 <uttambsdc2@gmail.com>
2021-01-25 02:03:56 -0500

    program complete

---
+++ Average_of_numbers.c
@@ -0,0 +1,34 @@
+/*
+*      File Name: Average_of_numbers.c
+
+*	      Date:25/01/2021
+*
+*      Tuitor: Uttam Kumar 
+*
+*      Email Id: uttambsdc2@gmail.com
+*/
+
+/* Description: C program to print Average of N numbers */
+
+#include<stdio.h>
+int main()
+{
+	int k, sum=0, i;
+
+	printf ("How many number'you want to average?\n Enter the number");
+	scanf ("%d",&k);
+
+	int a[k];
+	printf ("Enter %d numbers\n",k);
+
+	for (i=0; i<k; i++)
+	{
+		scanf ("%d", &a[i]);
+		sum = sum +a[i];
+	}
+	printf ("Sum is =%d\n",sum);
+	printf ("\nAverage of numbers is =%f\n",(float)sum/k);
+
+	return 0;
+
+}
